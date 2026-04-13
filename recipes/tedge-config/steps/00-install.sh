9f18a6cc69feb5d4ba35afc2f8d58574e5ef158e
reubenmiller <reuben.d.miller@gmail.com>
2026-04-13 19:25:20 +0200

    feat: remove tedge-config recipe

--- recipes/tedge-config/steps/00-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -n "$RECIPE_PARAM_MQTT_BIND_ADDRESS" ]; then
-    echo "Setting mqtt.bind.address to $RECIPE_PARAM_MQTT_BIND_ADDRESS" >&2
-    tedge config set mqtt.bind.address "$RECIPE_PARAM_MQTT_BIND_ADDRESS"
-fi
